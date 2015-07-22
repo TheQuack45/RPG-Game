bbfd86f4483b033fdc66073ff5c84dc2224284cb
Daniel Quackenbush <TheQuack45@gmail.com>
2015-07-22 10:32:23 -0400

    Began adding code for boss fighting at mountain

---
+++ RpgGame/RpgGame/Boss.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgGame
+{
+    class Boss
+    {
+        // Fields declaration
+        private string Name;
+        public string name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+        private int Damage;
+        public int damage
+        {
+            get { return Damage; }
+            set { Damage = value; }
+        }
+        private int Health;
+        public int health
+        {
+            get { return Health; }
+            set { Health = value; }
+        }
+        private int Level;
+        public int level
+        {
+            get { return Level; }
+            set { Level = value; }
+        }
+        private int SuperCritChance;
+        public int superCritChance
+        {
+            get { return SuperCritChance; }
+            set { SuperCritChance = value; }
+        }
+        private int EvadeChance;
+        public int evadeChance
+        {
+            get { return EvadeChance; }
+            set { EvadeChance = value; }
+        }
+        private bool IsEvaded;
+        public bool isEvaded
+        {
+            get { return IsEvaded; }
+            set { IsEvaded = value; }
+        }
+        private int SuperCritIncrement;
+        public int superCritIncrement
+        {
+            get { return SuperCritIncrement; }
+            set { SuperCritIncrement = value; }
+        }
+        private int SuperCritAmt;
+        public int superCritAmt
+        {
+            get { return SuperCritAmt; }
+        }
+
+        // Constructors
+        public Boss()
+        {
+
+        }
+
+        public Boss(string name, int damage, int health, int level, int superCritChance)
+        {
+            this.name = name;
+            this.damage = damage;
+            this.health = health;
+            this.level = level;
+            this.superCritChance = superCritChance;
+        }
+
+        // Methods definition
+        public void resetSuperCrit()
+        {
+            this.SuperCritAmt = 0;
+        }
+    }
+}
--- RpgGame/RpgGame/Mountain.cs
+++ RpgGame/RpgGame/Mountain.cs
@@ -8,6 +8,109 @@ namespace RpgGame
 {
     class Mountain
     {
+        // Variable declaration
+        Random randomNumberGen = new Random();
 
+        // Fields declaration
+
+        // Constructors
+
+        // Methods definition
+        public bool fight(Player gamePlayer, Boss fightBoss)
+        {
+            string choice;
+            bool isSuperCrit = false;
+
+            while (gamePlayer.health > 0 && fightBoss.health > 0)
+            {
+                // Check if supercrit occurred
+                if (fightBoss.superCritAmt == 100)
+                {
+                    if (randomNumberGen.Next(100) <= fightBoss.superCritChance)
+                    {
+                        // Super crit amount is 100 and chance passed
+                        isSuperCrit = true;
+                        fightBoss.resetSuperCrit();
+                    }
+                }
+                
+                // Fight content
+                Console.WriteLine("You have " + gamePlayer.health + " health and the boss has " + fightBoss.health + " health.");
+                Console.WriteLine("What do you want to do? (attack, block, potion, or run)");
+                choice = Console.ReadLine();
+                if (choice == "attack")
+                {
+                    // Check if supercrit occurred
+                    // Player chose to attack monster
+                    // Calc crit chance
+                    if (randomNumberGen.Next(100) <= gamePlayer.critChance)
+                    {
+                        // Player critted; do two times damage
+                        Console.WriteLine("Critical hit on the monster!");
+                        gamePlayer.health -= fightBoss.damage;
+                        fightBoss.health -= (gamePlayer.damage * 2);
+                    }
+                    else
+                    {
+                        // Player did not crit; do standard damage
+                        gamePlayer.health -= fightBoss.damage;
+                        fightBoss.health -= gamePlayer.damage;
+                    }
+                }
+                else if (choice == "block")
+                {
+                    // Player chose to block
+                    // Calc block chance
+                    if (randomNumberGen.Next(100) > gamePlayer.blockChance)
+                    {
+                        // Player successfully blocked; negate enemy damage
+                        Console.WriteLine("You blocked successfully and took no damage, as well as dealing some to the monster!");
+                        fightBoss.health -= (gamePlayer.damage / 2);
+                    }
+                    else
+                    {
+                        // Player did not block successfully; take damage and deal none
+                        Console.WriteLine("You did not block successfully!");
+                        gamePlayer.health -= fightBoss.damage;
+                    }
+                }
+                else if (choice == "potion")
+                {
+                    // Player chose to use potion
+                    // List potions in inventory
+
+                    // TODO: Add potion usage
+
+                }
+                else if (choice == "run")
+                {
+                    // Player chose to run
+                    // Calc evade chance
+                    if (randomNumberGen.Next(100) <= fightBoss.evadeChance)
+                    {
+                        // Player successfully evaded
+                        // Return to village
+                        fightBoss.health = 0;
+                        fightBoss.isEvaded = true;
+                    }
+                    else
+                    {
+                        // Player did not evade successfully. Take small damage and continue fight
+                        int evadeDamage = (fightBoss.damage / 3);
+                        Console.WriteLine("You were unable to escape the boss! You took " + evadeDamage + " damage.");
+                        gamePlayer.health -= evadeDamage;
+                    }
+                }
+            }
+
+            if (gamePlayer.health <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
     }
 }
